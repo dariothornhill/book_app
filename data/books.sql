7376e3f09d0a54938c40c9da4b211266d38254f8
Dario Thornhill <dario.thornhill@gmail.com>
2021-04-07 11:32:00 +0300

    Added show books logic

--- data/books.sql
+++ data/books.sql
@@ -1,6 +1,7 @@
 -- \c task_app;
 
 DROP TABLE IF EXISTS books;
+
 CREATE TABLE books (
   id SERIAL PRIMARY KEY,
   author VARCHAR(255),
@@ -10,5 +11,4 @@ CREATE TABLE books (
   description TEXT
 );
 
-INSERT INTO books (id, author, title, isbn, image_url, description) 
-VALUES(1, 'Reema', 'Mars', 'ISBN_13 9780441013593', 'imageURL', 'Occupy Mars');
+-- INSERT INTO books (id, author, title, isbn, image_url, description) VALUES(1, 'Reema', 'Mars', 'ISBN_13 9780441013593', 'imageURL', 'Occupy Mars');
--- seed.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO books (id, author, title, isbn, image_url, description) 
-VALUES(1, 'Reema', 'Mars', 'ISBN_13 9780441013593', 'imageURL', 'Occupy Mars');
